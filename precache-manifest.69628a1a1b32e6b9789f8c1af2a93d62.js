1418e7ad8ce27aaa318e3d467e8444ae581a02f6
Arobl034 <robles305@gmail.com>
2020-10-17 16:28:55 -0400

    Updates

---
+++ precache-manifest.69628a1a1b32e6b9789f8c1af2a93d62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d888570f44f4aea01cad7ff1cce180",
+    "url": "/Nested-components-react/index.html"
+  },
+  {
+    "revision": "fe8c9914512bec9b0946",
+    "url": "/Nested-components-react/static/css/main.f8a77616.chunk.css"
+  },
+  {
+    "revision": "1046851c02f0d76563ef",
+    "url": "/Nested-components-react/static/js/2.585fd38e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Nested-components-react/static/js/2.585fd38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8c9914512bec9b0946",
+    "url": "/Nested-components-react/static/js/main.899157db.chunk.js"
+  },
+  {
+    "revision": "c16cef6b8020d53b1154",
+    "url": "/Nested-components-react/static/js/runtime-main.508839ed.js"
+  }
+]);
